1d088ce3c6e720d9bd5cc824cc7cd704c579e0fa
Wyatt C <wcastellan@yahoo.com>
2021-10-17 10:57:26 -0400

    added var and console.log commands in javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?")
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+10)
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
